a2cd55d3007077e03e0eacf29611aa80eecf54d0
Suhail Shergill <suhailshergill@gmail.com>
2012-03-21 13:38:29 -0400

    adding a general memoization function
    [[http://www.maztravel.com/haskell/memofib.html][source]]

--- haskell/Su/Utils.hs
+++ haskell/Su/Utils.hs
@@ -1,6 +1,8 @@
 module Su.Utils where
 
 import System.Environment (getEnv)
+import qualified Control.Monad.State.Lazy as S
+import qualified Data.Map as M
 
 safeReadFile :: FilePath -> IO String
 safeReadFile = mkSafe readFile
@@ -19,3 +21,16 @@ mkSafe = mkSafe2 []
 
 mkSafe2 :: defaultValType -> (a -> IO defaultValType) -> a -> IO defaultValType
 mkSafe2 = (.) . flip catch . const . return
+
+
+type StateMap a b = S.State (M.Map a b) b
+
+memoizeM :: (Show a, Show b, Ord a) => 
+            ((a -> StateMap a b) -> (a -> StateMap a b)) -> (a -> b)
+memoizeM t x = S.evalState (f x) M.empty where
+  g x = do
+    y <- t f x  
+    m <- S.get
+    S.put $ M.insert x y m
+    return $ y
+  f x = S.get >>= \m -> maybe (g x) return (M.lookup x m)
